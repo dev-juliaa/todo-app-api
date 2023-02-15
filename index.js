b4f1c8614f866b87de36d0f7f53ace9de24c7b14
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-02-15 08:39:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
 
-app.get('usuario', (req, res) => {
-    res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados');
+app.get('/', (req, res) => {
+    res.send('Rota ativada com GET e valores de usuarios devem ser retornados');
 })
 
-app.get('tarefas', (req, res) => {
-    res.send('Rota ativada com GET e recurso tarefas: valores de tarefas devem ser retornados');
+app.get('/', (req, res) => {
+    res.send('Rota ativada com GET e valores de tarefas devem ser retornados');
 })
 
 app.listen(port, () => {
-    console.log(`porta ${port}`)
+    console.log(`porta ${port}`);
 })
+
+
+
 
 
 
